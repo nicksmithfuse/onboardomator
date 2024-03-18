6ca012cd8fe5463346cd8ff2cc6a0345183b4e9d
rhanns <rhannska@gmail.com>
2024-03-18 07:44:56 -0500

    got everything except state dropdown

--- main.py
+++ main.py
@@ -5,38 +5,42 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as ec
 from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.chrome.options import Options
 
 # Locally stored username and password hashtag security hashtag we did it
 mscan_username = os.getenv("mscanuser")
 mscan_password = os.getenv("mscanpw")
 
 # Create a new instance of the shiny and Chrome driver
-driver = webdriver.Chrome()
+chrome_options = Options()
+chrome_options.add_argument("--start-maximized")
+
+driver = webdriver.Chrome(options=chrome_options)
 
 # Navigate to the partner login page in chrome driver
 login_url = "https://portal.mscanapi.com/#/Partner/Login"
 driver.get(login_url)
 
 try:
-    # Wait for the partner ID input field to be interactable, and then enter the username variable
+    # Wait for the partner ID input field to be CLICKABLE, and then enter the username variable
     partner_id_input = WebDriverWait(driver, 10).until(
         ec.element_to_be_clickable((By.CSS_SELECTOR, "input[name='Partner ID']"))
     )
     partner_id_input.send_keys(mscan_username)
 
-    # Wait for the password input field to be interactable, and then enter the password variable
+    # Wait for the password input field to be CLICKABLE, and then enter the password variable
     password_input = WebDriverWait(driver, 10).until(
         ec.element_to_be_clickable((By.CSS_SELECTOR, "input[name='Password']"))
     )
     password_input.send_keys(mscan_password)
 
-    # Find and click the login button
+    # Find, wait for CLICKABLE, and click the login button
     login_button = WebDriverWait(driver, 10).until(
         ec.element_to_be_clickable((By.CSS_SELECTOR, "button.new-login-button"))
     )
     login_button.click()
 
-    # Wait for the loading backdrop to disappear
+    # Wait for the loading backdrop to disappear since this errored out twice
     WebDriverWait(driver, 10).until_not(
         ec.presence_of_element_located((By.CSS_SELECTOR, "div.q-loading__backdrop"))
     )
@@ -47,11 +51,16 @@ try:
     )
     create_account_button.click()
 
+    # Wait for the loading backdrop to disappear since this errored out twice and thrice again
+    WebDriverWait(driver, 10).until_not(
+        ec.presence_of_element_located((By.CSS_SELECTOR, "div.q-loading__backdrop"))
+    )
+
     # Wait for the "Dealership Name" input field to be interactable
     dealership_name_field_interactable = WebDriverWait(driver, 10).until(
         ec.element_to_be_clickable((By.CSS_SELECTOR, "input[aria-label='Dealership Name']"))
     )
-    # locate street, city, zip and phone now that the first one is interactable
+    # LOCATE street, city, zip and phone now that the first one is interactable
     street_field = WebDriverWait(driver, 10).until(
         ec.presence_of_element_located((By.CSS_SELECTOR, "input[aria-label='Street']"))
     )
@@ -65,14 +74,62 @@ try:
     country_dropdown_icon = WebDriverWait(driver, 20).until(
         ec.presence_of_element_located((By.CSS_SELECTOR, "i.q-icon.q-select__dropdown-icon"))
     )
-    # Click the "Country" dropdown icon
-    country_dropdown_icon.click()
-    # Wait for the "USA" option (set by finding it in inspect element but this isn't great) to be present and click it
+
+    # Select USA from the country dropdown
+    country_dropdown = WebDriverWait(driver, 20).until(
+        ec.presence_of_element_located((By.CSS_SELECTOR, "input[aria-label='Country']"))
+    )
+    country_dropdown.click()
     usa_option = WebDriverWait(driver, 20).until(
         ec.element_to_be_clickable(
             (By.XPATH, "//div[contains(@class, 'q-menu')]//div[contains(@class, 'q-item__label')]//span[text()='USA']"))
     )
     usa_option.click()
+
+        # # Wait for the state dropdown element to be present after selecting USA
+    # state_dropdown = WebDriverWait(driver, 20).until(
+    #     ec.presence_of_element_located(
+    #         (By.CSS_SELECTOR, "div.q-field.q-field--outlined.q-select.q-field--dense.q-field--float"))
+    # )
+    #
+    # # Click on the state dropdown to open it
+    # state_dropdown.click()
+    #
+    # # Wait for the state options to be visible and interactable
+    # state_options = WebDriverWait(driver, 20).until(
+    #     ec.presence_of_all_elements_located((By.CSS_SELECTOR, "div.q-item__label"))
+    # )
+    #
+    # # Find the desired state option based on the value from the CSV file and click on it
+    # desired_state = "CA"  # Replace with the actual state value from the CSV file
+    # for option in state_options:
+    #     if option.text == desired_state:
+    #         option.click()
+    #         break
+
+    # Target the "API Status" dropdown and select "Beta"
+    api_status_dropdown = WebDriverWait(driver, 20).until(
+        ec.presence_of_element_located((By.CSS_SELECTOR, "input[aria-label='API Status']"))
+    )
+    api_status_dropdown.click()
+    beta_option = WebDriverWait(driver, 20).until(
+        ec.element_to_be_clickable(
+            (By.XPATH,
+             "//div[contains(@class, 'q-menu')]//div[contains(@class, 'q-item__label')]//span[contains(text(), 'Beta')]"))
+    )
+    beta_option.click()
+
+    # Wait for the calendar field to be interactable and click it
+    calendar_field = WebDriverWait(driver, 20).until(
+        ec.element_to_be_clickable((By.CSS_SELECTOR, "i.q-icon.text-green.notranslate.material-icons.cursor-pointer"))
+    )
+    calendar_field.click()
+
+    # Wait for the "OK" button to be clickable and click it
+    ok_button = WebDriverWait(driver, 20).until(
+        ec.element_to_be_clickable((By.XPATH, "//span[contains(@class, 'block') and text()='OK']"))
+    )
+    ok_button.click()
 
     # used element to be clickable here as this one generates after USA selected in dropdown
     zip_field = WebDriverWait(driver, 10).until(
@@ -105,9 +162,9 @@ try:
                 zip_field.send_keys(zip_code)
 
 
-    # input keeps script from closing out browser. user continues manually from this point.
-    print("press enter to close even though you can't")
-    input()
+        # input keeps script from closing out browser. user continues manually from this point.
+        print("press enter to close even though you can't")
+        input()
 
 finally:
     # Close the browser
